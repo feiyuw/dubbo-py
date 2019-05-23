1dd3c974ff795e92687b3d911711f9196a2c6c0b
Zhang Yu <zhangyu@dingxiang-inc.com>
2019-05-23 16:05:04 +0800

    for python3 only

--- dubbo/__init__.py
+++ dubbo/__init__.py
@@ -1,8 +1,3 @@
-import sys
-if sys.version_info.major < 3:
-    raise RuntimeError('dubbo_py require python 3+')
-
-
 class long(int):
     pass
 
--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ CURDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 setup(
-    name='dubbo_py',
-    version='0.1',
+    name='dubbo-py',
+    version='0.2',
     description='dubbo adaptor for python',
     author='Zhang Yu',
     author_email='feiyuw@gmail.com',
-    url='https://github.com/feiyuw/dubbo_py.git',
+    url='https://github.com/feiyuw/dubbo-py.git',
+    python_requires='>=3.5',
     install_requires=['kazoo'],
     packages=[
         'dubbo',
